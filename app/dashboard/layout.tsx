94f606baa51cddf5f098dbb38af69e9f22578adb
sharp3614 <knurhak09@gmail.com>
2024-05-07 16:04:21 +0300

    page design updated

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,20 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+import Navbar from '../ui/dashboard/navbar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <div className="grid h-screen w-full overflow-hidden md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="bg-muted/40 hidden border-r md:block">
+        <div className="hidden h-full max-h-screen flex-col gap-2 md:flex">
+          <SideNav />
+        </div>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex h-full flex-1 flex-col gap-4 overflow-auto lg:gap-6">
+        <div className="bg-slate-400">
+          <Navbar />
+        </div>
+        <main className="p-4 lg:p-6">{children}</main>
+      </div>
     </div>
   );
 }
---
+++ app/ui/dashboard/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      
+    </header>
+  )
+}
+
+export default Navbar
